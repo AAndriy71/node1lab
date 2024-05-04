dab18b043b0ff731ee43fd0bc17148d6d13df7ca
AAndriy71 <152483135+AAndriy71@users.noreply.github.com>
2024-05-04 09:33:50 +0300

    Create task03.js

---
+++ Lab1/playground/task03.js
@@ -0,0 +1,8 @@
+const userName = 'ANDRIY';
+
+// Виведення інформації про користувача
+console.log(`Інформація про користувача ${userName}:`);
+console.log(`Ім'я користувача: ${userName}`);
+
+// Виведення привітання
+console.log(`Hello, ${userName}!`);
